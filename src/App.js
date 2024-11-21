6c5d08afe2f85247939a978efb576201df71f720
Preston Bateman <prestonbbateman@gmail.com>
2024-11-21 15:26:53 -0600

    created components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
+  const [books, setBooks] = useState([])
+
+  const createBook = () => {
+
+  }
+
   return (
     <div className="App">
-      
+      <h1>Reading List</h1>
+      <BookList />
+      <BookCreate  />
     </div>
   );
 }
