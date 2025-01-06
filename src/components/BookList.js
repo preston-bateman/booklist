f59e3b7e7fe242a29b4e49e6a8bc45ba636be2c5
Preston Bateman <prestonbbateman@gmail.com>
2025-01-05 21:20:13 -0600

    changes

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -4,6 +4,8 @@ function BookList ({ books, onDelete, onEdit }) {
     const renderedBooks = books.map((book) => {
         return <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />
     })
+    
+
 
     return(
         <div className="book-list">
